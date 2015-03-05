69d38a45d2caeba4d92f34fa09c81ac74cef2407
Nikolay Dionisov <nikolaydionisov@gmail.com>
2015-03-05 08:37:07 +0200

    Get stream data

--- src/eventorg/stream.clj
+++ src/eventorg/stream.clj
@@ -29,35 +29,40 @@
       (send! receiver (response {:msg msg}))
       nil)))
 
-  (-> (r/db db-name)
-      (r/table-db "streams")
-      (r/filter {:id "f3be18bf-408f-451f-8d10-e2fee82bb1a9" :write :true})
-      (r/pluck ["tags" "user"])
-      (r/merge {:time (r/now) :value "TestData"})
-      (r/foreach (r/lambda [record]
-                    (-> (r/db db-name)
-                        (r/table-db "events")
-                        (r/insert record))))
-      (run local-conn))
-
-(defn api-streams-post [{req :params}]
+(defn api-streams-post
+  "Post a new event to a stream"
+  [{req :params}]
   (prn req)
   (if (get (first (get (-> (r/db db-name)
-                  (r/table-db "streams")
-                  (r/filter {:id (:id req) :write :true})
-                  (r/pluck ["tags" "user"])
-                  (r/merge {:time (r/now) :value (dissoc req :id)})
-                  (r/foreach (r/lambda [record]
-                                       (-> (r/db db-name)
-                                           (r/table-db "events")
-                                           (r/insert record))))
-                  (run local-conn)) :response nil )) :inserted nil)
+                           (r/table-db "streams")
+                           (r/filter {:id (:id req) :write :true})
+                           (r/pluck ["tags" "user"])
+                           (r/merge {:time (r/now) :value (dissoc req :id)})
+                           (r/foreach (r/lambda [record]
+                                                (-> (r/db db-name)
+                                                    (r/table-db "events")
+                                                    (r/insert record))))
+                           (run local-conn)) :response nil )) :inserted nil)
     {:status 200  :headers {}   :body {:success :true}}
     {:status 401  :headers {}   :body {:success :false}}))
 
 
-(defn api-streams-get [{r :params}]
-  (response (get @working-data (:id r) [])))
+
+
+(defn api-streams-get
+  "Get all data in a stream"
+  [{req :params}]
+  (prn req)
+  (response (:response (-> (r/db db-name)
+                          (r/table-db "events")
+                          (r/filter (r/lambda [event]
+                                              (r/>= (-> (r/db db-name)
+                                                  (r/table-db "streams")
+                                                  (r/filter (r/lambda [stream]
+                                                                      (r/and (r/= (r/get-field stream :user) (r/get-field event :user))
+                                                                             (r/= (r/get-field stream :id) (:id req)))))
+                                                  (r/count)) 1)))
+                          (run local-conn)))))
 
 
 
